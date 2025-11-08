c040b69e0a737af401fe4df75231e692a33d003b
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-07 19:49:27 -0800

    Update 12

--- 210-lab-29.cpp
+++ 210-lab-29.cpp
@@ -45,8 +45,8 @@ bool eventOccurs(int);
 
 // Define main function
 int main() {
-	//srand(time(0));
-	srand(3);
+	srand(time(0));
+
 	// Initialize a map to store the flower shop's data: customers who come in and what they order, flowers currently in
 	// the flower shop inventory, and flowers growing in the supplier's greenhouse.
 	array<list<string>, SIZE> shop1data = {};
@@ -71,11 +71,9 @@ int main() {
 		cout << "Error opening file." << endl;
 	}
 
-	// Begin a time-based simulation for running the flower shop
-	// for 35 time intervals (i.e. hours shop is open during the week)
+	cout << "Beginning a simulation for 35 intervals (i.e. hours shop is open during a week):" << endl;
 	runSimulation(shops, 35);
 
-	// End of main function
 	return 0;
 }
 
@@ -85,16 +83,18 @@ void runSimulation(map<string, array<list<string>, SIZE> >& storeData, int inter
 	// For n number of time periods
 	for (int i = 0; i < intervals; i++) {
 		cout << "Interval #" << i + 1 << ':' << endl;
+		bool anyEventOccured = false;
 
 		// EVENT #1: Randomly decide if a customer will arrive (50% chance)
 		if (eventOccurs(50)) {
+			anyEventOccured = true;
 			// If they arrive, randomly decide what they order
 			string order = getRandomFlower();
 			bool found = false;
 			auto it = storeData.at("shop1")[0].begin();
 			while (!found && it != storeData.at("shop1")[0].end()) {
 				if (*it == order) { // If the flowers needed for the customer's order are available, remove the flowers from the shop's inventory
-					cout << "\tCustomer ordered " << order << " and 1 " << *it << " was removed from the store's inventory." << endl;
+					cout << "\tCustomer ordered 1 " << order << " and 1 " << *it << " was removed from the store's inventory." << endl;
 					storeData.at("shop1")[0].erase(it);
 					found = true;
 					break;
@@ -105,14 +105,15 @@ void runSimulation(map<string, array<list<string>, SIZE> >& storeData, int inter
 			if (!found) {
 				// Otherwise, add the customer to the queue and the flower needed for completing their order to the
 				// end of the supplier's greenhouse list
-				cout << "\tCustomer ordered " << order << " and it was not in stock. A request has been made to the supplier and the customer has been added to the queue" << endl;
+				cout << "\tCustomer ordered 1 " << order << " and it was not in stock. A request has been made to the supplier and the customer has been added to the queue" << endl;
 				storeData.at("shop1")[1].push_back(order);
 				storeData.at("shop1")[2].push_back(order);
 			}
 		}
 
 		// EVENT #2: Randomly decide if a flower in the supplier's greenhouse are ready to deliver (75% chance)
-		if (eventOccurs(75)) {
+		if (eventOccurs(75) && !storeData.at("shop1")[1].empty()) {
+			anyEventOccured = true;
 			string delivered = storeData.at("shop1")[1].front();
 			storeData.at("shop1")[0].push_back(delivered);
 			storeData.at("shop1")[1].pop_front();
@@ -120,9 +121,10 @@ void runSimulation(map<string, array<list<string>, SIZE> >& storeData, int inter
 			cout << "\t1 " << delivered << " was added to the store inventory from the greenhouse." << endl;
 		}
 
-		// EVENT #3: If there are customers left in the queue, randomly decide to check if their order can now be made. (50%)
-		if (eventOccurs(50)) {
+		// EVENT #3: If there are customers left in the queue, randomly decide to check if their order can now be made. (75%)
+		if (eventOccurs(75)) {
 			if (!storeData.at("shop1")[2].empty()) {
+				anyEventOccured = true;
 				// Give customer their order if it's available
 				for (auto it = storeData.at("shop1")[0].begin(); it != storeData.at("shop1")[0].end(); ++it) { // TODO: Fix so it's not hardcoded
 					if (*it == storeData.at("shop1")[2].front()) { // If the first customer in the queue's order is in stock
@@ -135,8 +137,11 @@ void runSimulation(map<string, array<list<string>, SIZE> >& storeData, int inter
 			}
 		}
 
+		if (!anyEventOccured)
+			cout << "\tNothing happened." << endl;
+
 		// Wait or pause briefly to simulate the passage of time between intervals
-		this_thread::sleep_for(chrono::seconds(1));
+		this_thread::sleep_for(chrono::milliseconds(500));
 	}
 }
 
