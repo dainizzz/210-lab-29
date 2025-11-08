255670b111fafbff60454bb9fc8f1dd63261d031
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-07 17:27:43 -0800

    Update 3

--- 210-lab-29.cpp
+++ 210-lab-29.cpp
@@ -11,37 +11,39 @@
 using namespace std;
 
 const int SIZE = 3;
-const int FLOWERS = 15;
-const string flowers[FLOWERS] = {
-"Rose",
-"Tulip",
-"Lily",
-"Carnation",
-"Sunflower",
-"Orchid",
-"Daisy",
-"Chrysanthemum",
-"Peony",
-"Hydrangea",
-"Bluebell",
-"Alstroemeria",
-"Iris",
-"Freesia",
-"Ranunculus",
+const int NUM_FLOWERS = 15;
+const string flowers[NUM_FLOWERS] = {
+	"Rose",
+	"Tulip",
+	"Lily",
+	"Carnation",
+	"Sunflower",
+	"Orchid",
+	"Daisy",
+	"Chrysanthemum",
+	"Peony",
+	"Hydrangea",
+	"Bluebell",
+	"Alstroemeria",
+	"Iris",
+	"Freesia",
+	"Ranunculus",
 };
 
 // Define a function to simulate customer orders, store orders to the supplier, and supplier deliveries
-	// Parameters: map of store data, number of intervals
+// Parameters: map of store data, number of intervals
 
 // Define main function
-int main () {
+int main() {
 	// Initialize a map to store the flower shop's data: customers who come in and what they order, flowers currently in
 	// the flower shop inventory, and flowers growing in the supplier's greenhouse.
 	list<string> shopInventory = {};
 	list<string> greenhouseInventory = {};
 	list<string> customerQueue = {};
+	array<list<string>, SIZE> shop1data = {shopInventory, greenhouseInventory, customerQueue};
+	map<string, array<list<string>, SIZE>> shops;
 
-	array<list<string>, SIZE> data = {shopInventory, greenhouseInventory, customerQueue};
+	shops.insert(make_pair("shop 1"),shop1data);
 
 	// Open an external file to read initial data about flower shop inventory and greenhouse inventory
 	ifstream infile("flower inventory.txt");
@@ -50,21 +52,21 @@ int main () {
 		while (infile >> tempLoc) {
 			infile >> tempFlower;
 			if (tempLoc == "shop")
-				shopInventory.push_back(tempFlower);
+				shopData[1].push_back(tempFlower);
 			else
-				greenhouseInventory.push_back(tempFlower);
+				shopData[2].push_back(tempFlower);
 		}
 		infile.close();
-	}else {
+	} else {
 		// If file does not open, print an error and exit
 		cout << "Error opening file." << endl;
 	}
 
-	for (string flower : shopInventory) {
+	for (string flower: data[1]) {
 		cout << flower << " ";
-    }
+	}
 
-	for (string flower : greenhouseInventory) {
+	for (string flower: greenhouseInventory) {
 		cout << flower << " ";
 	}
 
@@ -74,8 +76,8 @@ int main () {
 	// for 35 time intervals (i.e. hours shop is open during the week)
 
 	// Read data from file and populate map
-		// For each line, extract the flower's name and whether it goes to the flower shop or the greenhouse
-		// Insert flower into the appropriate list in the array for the shop
+	// For each line, extract the flower's name and whether it goes to the flower shop or the greenhouse
+	// Insert flower into the appropriate list in the array for the shop
 	// Close the file
 
 	// End of main function
@@ -83,15 +85,15 @@ int main () {
 }
 
 // Define simulation function
-	// For n number of time periods
-		// Randomly decide if a customer will arrive
-			// If they arrive, randomly decide what they order
-				// If the flowers needed for the customer's order are available, remove the flowers from the shop's
-				// inventory
-				// Otherwise, add the customer to the queue and the necessary flowers for completing their order to the
-				// end of the supplier's greenhouse list
-		// Randomly decide if flowers in the supplier's greenhouse are ready to deliver
-			// If so, add them to the store's inventory
-		// If there are customers left in the queue, randomly decide if their order can now be made.
-	// Print the changes for this interval, e.g., "Customer ordered {order} and {flowers} were removed from the shop's inventory"
-	// Wait or pause briefly to simulate the passage of time between intervals
+// For n number of time periods
+// Randomly decide if a customer will arrive
+// If they arrive, randomly decide what they order
+// If the flowers needed for the customer's order are available, remove the flowers from the shop's
+// inventory
+// Otherwise, add the customer to the queue and the necessary flowers for completing their order to the
+// end of the supplier's greenhouse list
+// Randomly decide if flowers in the supplier's greenhouse are ready to deliver
+// If so, add them to the store's inventory
+// If there are customers left in the queue, randomly decide if their order can now be made.
+// Print the changes for this interval, e.g., "Customer ordered {order} and {flowers} were removed from the shop's inventory"
+// Wait or pause briefly to simulate the passage of time between intervals
