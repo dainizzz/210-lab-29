8f43239b7c1bf545b158500ceb9d6a1efe2443fb
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-07 20:00:35 -0800

    Update 13

--- 210-lab-29.cpp
+++ 210-lab-29.cpp
@@ -15,26 +15,14 @@ using namespace std;
 const int SIZE = 3;
 const int NUM_FLOWERS = 15;
 const string flowers[NUM_FLOWERS] = {
-	"Rose",
-	"Tulip",
-	"Lily",
-	"Carnation",
-	"Sunflower",
-	"Orchid",
-	"Daisy",
-	"Chrysanthemum",
-	"Peony",
-	"Hydrangea",
-	"Bluebell",
-	"Alstroemeria",
-	"Iris",
-	"Freesia",
-	"Ranunculus",
+	"Rose", "Tulip", "Lily", "Carnation", "Sunflower",
+	"Orchid", "Daisy", "Chrysanthemum", "Peony", "Hydrangea",
+	"Bluebell", "Alstroemeria", "Iris", "Freesia", "Ranunculus",
 };
 
 // Define a function to simulate customer orders, store orders to the supplier, and supplier deliveries
 // Parameters: map of store data, number of intervals
-void runSimulation(map<string, array<list<string>, SIZE> >&, int);
+void runSimulation(map<string, array<list<string>, SIZE> > &, int);
 
 // Returns a randomly selected flower from the flowers array.
 string getRandomFlower();
@@ -50,9 +38,9 @@ int main() {
 	// Initialize a map to store the flower shop's data: customers who come in and what they order, flowers currently in
 	// the flower shop inventory, and flowers growing in the supplier's greenhouse.
 	array<list<string>, SIZE> shop1data = {};
-	map<string, array<list<string>, SIZE>> shops;
+	map<string, array<list<string>, SIZE> > shops;
 
-	shops.insert(make_pair("shop1", shop1data));
+	shops.insert(make_pair("Shop 1", shop1data));
 
 	// Open an external file to read initial data about flower shop inventory and greenhouse inventory
 	ifstream infile("flower inventory.txt");
@@ -61,9 +49,9 @@ int main() {
 		while (infile >> tempLoc) {
 			infile >> tempFlower;
 			if (tempLoc == "shop")
-				shops.at("shop1")[0].push_back(tempFlower);
+				shops.at("Shop 1")[0].push_back(tempFlower);
 			else
-				shops.at("shop1")[1].push_back(tempFlower);
+				shops.at("Shop 1")[1].push_back(tempFlower);
 		}
 		infile.close();
 	} else {
@@ -78,70 +66,77 @@ int main() {
 }
 
 // Define simulation function
-void runSimulation(map<string, array<list<string>, SIZE> >& storeData, int intervals) {
-	// TODO: ADD FOR SHOP IN SHOPS
-	// For n number of time periods
-	for (int i = 0; i < intervals; i++) {
-		cout << "Interval #" << i + 1 << ':' << endl;
-		bool anyEventOccured = false;
+void runSimulation(map<string, array<list<string>, SIZE> > & shops, int intervals) {
+	for (auto &shop: shops) {
+		cout << shop.first << " simulation:" << endl;
+		for (int i = 0; i < intervals; i++) {
+			cout << "Interval #" << i + 1 << ':' << endl;
+			bool anyEventOccured = false;
 
-		// EVENT #1: Randomly decide if a customer will arrive (50% chance)
-		if (eventOccurs(50)) {
-			anyEventOccured = true;
-			// If they arrive, randomly decide what they order
-			string order = getRandomFlower();
-			bool found = false;
-			auto it = storeData.at("shop1")[0].begin();
-			while (!found && it != storeData.at("shop1")[0].end()) {
-				if (*it == order) { // If the flowers needed for the customer's order are available, remove the flowers from the shop's inventory
-					cout << "\tCustomer ordered 1 " << order << " and 1 " << *it << " was removed from the store's inventory." << endl;
-					storeData.at("shop1")[0].erase(it);
-					found = true;
-					break;
-				}
-				++it;
-			}
-
-			if (!found) {
-				// Otherwise, add the customer to the queue and the flower needed for completing their order to the
-				// end of the supplier's greenhouse list
-				cout << "\tCustomer ordered 1 " << order << " and it was not in stock. A request has been made to the supplier and the customer has been added to the queue" << endl;
-				storeData.at("shop1")[1].push_back(order);
-				storeData.at("shop1")[2].push_back(order);
-			}
-		}
-
-		// EVENT #2: Randomly decide if a flower in the supplier's greenhouse are ready to deliver (75% chance)
-		if (eventOccurs(75) && !storeData.at("shop1")[1].empty()) {
-			anyEventOccured = true;
-			string delivered = storeData.at("shop1")[1].front();
-			storeData.at("shop1")[0].push_back(delivered);
-			storeData.at("shop1")[1].pop_front();
-			// If so, add them to the store's inventory
-			cout << "\t1 " << delivered << " was added to the store inventory from the greenhouse." << endl;
-		}
-
-		// EVENT #3: If there are customers left in the queue, randomly decide to check if their order can now be made. (75%)
-		if (eventOccurs(75)) {
-			if (!storeData.at("shop1")[2].empty()) {
+			// EVENT #1: Randomly decide if a customer will arrive (50% chance)
+			if (eventOccurs(50)) {
 				anyEventOccured = true;
-				// Give customer their order if it's available
-				for (auto it = storeData.at("shop1")[0].begin(); it != storeData.at("shop1")[0].end(); ++it) { // TODO: Fix so it's not hardcoded
-					if (*it == storeData.at("shop1")[2].front()) { // If the first customer in the queue's order is in stock
-						cout << "\tCustomer's " << *it << " order was ready and 1 " << *it <<" was removed from the store's inventory." << endl;
-						storeData.at("shop1")[0].erase(it);
-						storeData.at("shop1")[2].pop_front();
+				// If they arrive, randomly decide what they order
+				string order = getRandomFlower();
+				bool found = false;
+				auto it = shop.second[0].begin();
+				while (!found && it != shop.second[0].end()) {
+					if (*it == order) {
+						// If the flowers needed for the customer's order are available, remove the flowers from the shop's inventory
+						cout << "\tCustomer ordered 1 " << order << " and 1 " << *it <<
+								" was removed from the store's inventory." << endl;
+						shop.second[0].erase(it);
+						found = true;
 						break;
+					}
+					++it;
+				}
+
+				if (!found) {
+					// Otherwise, add the customer to the queue and the flower needed for completing their order to the
+					// end of the supplier's greenhouse list
+					cout << "\tCustomer ordered 1 " << order <<
+							" and it was not in stock. A request has been made to the supplier and the customer has been added to the queue"
+							<< endl;
+					shop.second[1].push_back(order);
+					shop.second[2].push_back(order);
+				}
+			}
+
+			// EVENT #2: Randomly decide if a flower in the supplier's greenhouse are ready to deliver (75% chance)
+			if (eventOccurs(75) && !shop.second[1].empty()) {
+				anyEventOccured = true;
+				string delivered = shop.second[1].front();
+				shop.second[0].push_back(delivered);
+				shop.second[1].pop_front();
+				// If so, add them to the store's inventory
+				cout << "\t1 " << delivered << " was added to the store inventory from the greenhouse." << endl;
+			}
+
+			// EVENT #3: If there are customers left in the queue, randomly decide to check if their order can now be made. (75%)
+			if (eventOccurs(75)) {
+				if (!shop.second[2].empty()) {
+					anyEventOccured = true;
+					// Give customer their order if it's available
+					for (auto it = shop.second[0].begin(); it != shop.second[0].end(); ++it) {
+						if (*it == shop.second[2].front()) {
+							// If the first customer in the queue's order is in stock
+							cout << "\tCustomer's " << *it << " order was ready and 1 " << *it <<
+									" was removed from the store's inventory." << endl;
+							shop.second[0].erase(it);
+							shop.second[2].pop_front();
+							break;
+						}
 					}
 				}
 			}
+
+			if (!anyEventOccured)
+				cout << "\tNothing happened." << endl;
+
+			// Wait or pause briefly to simulate the passage of time between intervals
+			this_thread::sleep_for(chrono::milliseconds(500));
 		}
-
-		if (!anyEventOccured)
-			cout << "\tNothing happened." << endl;
-
-		// Wait or pause briefly to simulate the passage of time between intervals
-		this_thread::sleep_for(chrono::milliseconds(500));
 	}
 }
 
